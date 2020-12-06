749267d4e62f7c34760b7c9316bff760ea170ef8
szareckii <zareckii@gmail.com>
2020-12-06 21:58:43 +0300

    Homework6 +Room +NetworkStatus +Cache

--- app/src/main/java/com/szareckii/popularlibraries/ui/image/GlideImageLoader.kt
+++ app/src/main/java/com/szareckii/popularlibraries/ui/image/GlideImageLoader.kt
@@ -9,7 +9,7 @@ import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.szareckii.popularlibraries.mvp.model.image.IImageLoader
 
-class GlideImageLoader(): IImageLoader<ImageView> {
+class GlideImageLoader: IImageLoader<ImageView> {
 
     override fun loadInto(url: String, container: ImageView) {
         Glide.with(container.context)
